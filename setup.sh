aa7f77ead975ab2e149fd5c1811a7d304d59ec24
Daniel Schimpfoessl <dschimpf@redhat.com>
2022-08-19 12:45:56 -0500

    Adding S3 bucket automation

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+project=recipevault
+bucket=recipevault-images
 oc wait --for=delete ns/recipevault -A
 oc new-project recipevault
 
@@ -10,18 +12,19 @@ oc wait --for=jsonpath='{.status.phase}'=Running pod -l name=postgresql
 # S3 storage
 perl -pe "s|(http(s)?://).*$|http://$(oc get route/s3 -n openshift-storage -o jsonpath='{.spec.host}')|" -i deploy/rest-recipe/rest-recipe-deployment.yaml
 oc apply -f deploy/s3/obc.yaml
-perl -pe "s|(\"Resource\": \".*?:::)[^\"/]*|\$1recipevault-images|" -i deploy/s3/public_s3.json
-export AWS_ACCESS_KEY_ID=$(oc get secret --output json recipevault-images -n recipevault | jq -r .data.AWS_ACCESS_KEY_ID | base64 --decode)
-export AWS_SECRET_ACCESS_KEY=$(oc get secret --output json recipevault-images -n recipevault | jq -r .data.AWS_SECRET_ACCESS_KEY | base64 --decode)
+oc wait --for=jsonpath='{.status.phase}'=Bound obc/$bucket
+perl -pe "s|(\"Resource\": \".*?:::)[^\"/]*|\$1$bucket|" -i deploy/s3/public_s3.json
+export AWS_ACCESS_KEY_ID=$(oc get secret -o go-template --template="{{.data.AWS_ACCESS_KEY_ID|base64decode}}" $bucket)
+export AWS_SECRET_ACCESS_KEY=$(oc get secret -o go-template --template="{{.data.AWS_SECRET_ACCESS_KEY|base64decode}}" $bucket)
 s3route=$(oc get route/s3 -n openshift-storage -o jsonpath='{.spec.host}')
 
-aws s3api put-bucket-policy --no-verify-ssl --endpoint-url "https://$s3route" --bucket recipevault-images --policy "$(cat deploy/s3/public_s3.json)"
+aws s3api put-bucket-policy --no-verify-ssl --endpoint-url "https://$s3route" --bucket $bucket --policy "$(cat deploy/s3/public_s3.json)"
 
 # backend
 oc apply -f deploy/rest-recipe/
 oc wait --for=jsonpath='{.status.phase}'=Running pod -l app=rest-recipe
 
 # frontend
-perl -pe "s|(VUE_APP_IMAGE_SERVER_URL:).*$|\$1 http://$(oc get route/s3 -n openshift-storage -o jsonpath='{.spec.host}')/recipevault-images/|" -i deploy/ui-recipevault/recipevault-ui-config.yaml
+perl -pe "s|(VUE_APP_IMAGE_SERVER_URL:).*$|\$1 http://$(oc get route/s3 -n openshift-storage -o jsonpath='{.spec.host}')/$bucket/|" -i deploy/ui-recipevault/recipevault-ui-config.yaml
 perl -pe "s|(VUE_APP_RECIPE_DATA_SERVICE:).*$|\$1 https://$(oc get route/rest-recipe -o jsonpath='{.spec.host}')|" -i deploy/ui-recipevault/recipevault-ui-config.yaml
 oc apply -f deploy/ui-recipevault/
