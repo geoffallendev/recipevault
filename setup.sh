899ac14d4749499f1bcfcfc41aaf5f8b78ed6fd7
Daniel Schimpfoessl <dschimpf@redhat.com>
2022-08-19 09:23:34 -0500

    Adding S3 automation

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ oc new-app --name=postgresql --template=postgresql-persistent -p DATABASE_SERVIC
 perl -pe "s|(VUE_APP_IMAGE_SERVER_URL:).*$|\$1 http://$(oc get route/s3 -n openshift-storage -o jsonpath='{.spec.host}')/recipevault-images/|" -i deploy/ui-recipevault/recipevault-ui-config.yaml
 perl -pe "s|(VUE_APP_RECIPE_DATA_SERVICE:).*$|\$1 https://$(oc get route/rest-recipe -o jsonpath='{.spec.host}')|" -i deploy/ui-recipevault/recipevault-ui-config.yaml
 perl -pe "s|(http(s)?://).*$|http://$(oc get route/s3 -n openshift-storage -o jsonpath='{.spec.host}')|" -i deploy/rest-recipe/rest-recipe-deployment.yaml
+perl -pe "s|(\"Resource\": \".*?:::)[^\"/]*|\$1recipevault-images|" -i deploy/s3/public_s3.json
 oc apply -f deploy/s3/obc.yaml
+aws s3api put-bucket-policy --no-verify-ssl --endpoint-url https://s3-openshift-storage.apps.ocp.webwim.com --bucket recipevault-images --policy "$(cat deploy/s3/public_s3.json)"
 oc apply -f deploy/rest-recipe/
 oc apply -f deploy/ui-recipevault/
